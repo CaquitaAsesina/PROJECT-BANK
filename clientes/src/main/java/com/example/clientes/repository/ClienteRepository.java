5abd1311f11334d6bafb32b092c347f5fc92dda2
Jhostin Alvarez <dotatinker53@gmail.com>
2026-05-01 22:10:04 -0500

    repository.cliente version 1

--- clientes/src/main/java/com/example/clientes/repository/ClienteRepository.java
+++ clientes/src/main/java/com/example/clientes/repository/ClienteRepository.java
@@ -1,5 +1,22 @@
 package com.example.clientes.repository;
 
-public interface ClienteRepository {
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+import com.example.clientes.entities.Cliente;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface ClienteRepository extends ReactiveMongoRepository<Cliente, String> {
+    Mono<Cliente> findByEmail(String email);
+
+    Mono<Cliente> findByDni(String dni);
+
+    Flux<Cliente> findByEstado(Cliente.EstadoCliente estado);
+
+    Mono<Boolean> existsByEmail(String email);
+
+    Mono<Boolean> existsByDni(String dni);
 }
