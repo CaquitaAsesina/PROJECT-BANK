485ee1ae5aacc30864fd1681e0a1251ab053fbe4
Jhostin Alvarez <dotatinker53@gmail.com>
2026-05-04 22:39:50 -0500

    config.cliente

--- clientes/src/main/java/com/example/clientes/config/ClienteConfig.java
+++ clientes/src/main/java/com/example/clientes/config/ClienteConfig.java
@@ -1,5 +1,10 @@
 package com.example.clientes.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+
+@Configuration
+@EnableReactiveMongoAuditing
 public class ClienteConfig {
-    
+
 }
